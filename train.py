fd2b8ee00f806e97a810c61bbed5ebf394124880
lengstrom <lengstrom@users.noreply.github.com>
2015-11-19 18:22:04 -0800

    added

--- train.py
+++ train.py
@@ -5,9 +5,9 @@ import dejavu
 def copy_all_wavs_to_dir(olddir='./songs', newdir='./fp_wavs'):
     for yt_id in filter(lambda x: x[0] != '.', os.listdir(olddir)):
         fp = os.path.join(olddir, yt_id, 'song.wav')
-        shutil.copy(fp, os.path.join(newdir, yt_id + '.wav'))
+        new_fp = os.path.join(newdir, yt_id)
+        print fp, new_fp
+        shutil.copy(fp, new_fp)
     
 if 'copy' in sys.argv:
     copy_all_wavs_to_dir()
-
-
