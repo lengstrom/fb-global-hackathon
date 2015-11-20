6b137f4e65b2562bd6a57191c2231ea4aedaa06d
lengstrom <lengstrom@users.noreply.github.com>
2015-11-19 16:49:34 -0800

    added

--- get_song_info.py
+++ get_song_info.py
@@ -1,12 +1,20 @@
-import sys, os, json, requests, parse_srt
+import sys, os, json, requests, parse_srt, pdb
 from unicodedata import normalize
 SONG_ID=sys.argv[1]
 SONG_TITLE=sys.argv[2]
 SONG_ARTIST=sys.argv[3]
 GOOGLE_API_KEY=sys.argv[4]
 
-def make_srt(lang, vid_id):
-    xml = requests.get('http://video.google.com/timedtext?lang=' + lang + '&v=' + vid_id).text
+def make_srt(lang, vid_id, und_is_en, GB_is_en):
+    lang_ = lang
+    if lang == 'en' and und_is_en:
+        lang_ = 'und'
+
+    if lang == 'en' and GB_is_en:
+        lang_ = 'en-GB'
+
+
+    xml = requests.get('http://video.google.com/timedtext?lang=' + lang_ + '&v=' + vid_id).text
     try:
         xml = normalize('NFKD', xml).encode("ASCII", 'ignore')
     except:
@@ -23,15 +31,35 @@ items=filter(lambda x: x['kind'] == 'youtube#caption',items)
 json_to_write = {
     'title':SONG_TITLE,
     'artist':SONG_ARTIST,
-    'captions':[] #lang
+    'languages':[] #lang
 }
 
 for i in items:
     snip = i['snippet']
-    json_to_write['captions'].append(json_to_write['language'])
+    json_to_write['languages'].append(str(snip['language']))
+
+und_is_en = False
+GB_is_en = False
+
+json_to_write['languages'] = filter(lambda x: x in ['en', 'und', 'it', 'es', 'fr', "en-GB", 'es-MX', 'en-US', 'pt-BR'], json_to_write['languages'])
+
+if 'und' in json_to_write['languages'] and 'en' in json_to_write['languages']:
+    del json_to_write['languages'][json_to_write['languages'].index('und')]
+elif 'und' in json_to_write['languages'] and not 'en' in json_to_write['languages']:
+    json_to_write['languages'].append('en')
+    del json_to_write['languages'][json_to_write['languages'].index('und')]
+    und_is_en = True
+
+if 'en-GB' in json_to_write['languages'] and 'en' in json_to_write['languages']:
+    del json_to_write['languages'][json_to_write['languages'].index('en-GB')]
+elif 'en-GB' in json_to_write['languages'] and not 'en' in json_to_write['languages']:
+    json_to_write['languages'].append('en')
+    del json_to_write['languages'][json_to_write['languages'].index('en-GB')]
+    GB_is_en = True
+
+    
+for i in json_to_write['languages']:
+    make_srt(i, SONG_ID, und_is_en, GB_is_en)
 
 with open('song.json', 'w') as f:
     f.write(json.dumps(json_to_write))
-
-for i in json_to_write['captions']:
-    make_srt(i, SONG_ID)
--- get_vid.sh
+++ get_vid.sh
@@ -4,8 +4,10 @@
 # wav file
 # english srt file
 cd songs
+rm -rf $1
 mkdir $1
 cd $1
-python ./get_song_info.py $1 $2 $3 $GOOGLE_API_KEY #song.json
+python ../../get_song_info.py "$1" "$2" "$3" "$GOOGLE_API_KEY" #song.json
 youtube-dl --extract-audio --audio-format wav "https://www.youtube.com/watch?v=${1}" -o song.wav #song.wav
-
+ls 
+cd ../..
